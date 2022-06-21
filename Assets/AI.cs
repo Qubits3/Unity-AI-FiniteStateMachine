64d9150f54022f37f1bda3b3c6e9faa051091b9c
Metin Yıldıran <metin.1464@gmail.com>
2022-06-21 20:57:18 +0300

    Added Idle State to AI

---
+++ Assets/AI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    State currentState;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        currentState = new Idle(gameObject, agent, anim, player);
+    }
+
+    private void Update()
+    {
+        currentState = currentState.Process();
+    }
+}
